175981ddb30b0c96fc51c8207ae1632ccff002c4
Arathes <studium@fabianhammerschmidt.de>
2021-03-20 14:54:48 +0100

    Added: CheckWin() Method, Paint() Methode implemented

--- VierGewinnt.java
+++ VierGewinnt.java
@@ -6,6 +6,9 @@
 public class VierGewinnt {
     public static void main(String[] args) {
 
+        System.out.println("Welcome to the best command line implementation of \"Connect Four\"!");
+
+        Spielbrett brett = new Spielbrett(8,8);
     }
     public char[][] spielbrett;
     public VierGewinnt() {
