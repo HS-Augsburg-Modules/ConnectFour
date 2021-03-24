1ac6133f839583f4f5e32dcf4dc7045ad1c85044
Bienenwolf <happychris1@gmx.de>
2021-03-24 14:53:55 +0100

    Zeile 25 verbreitert
    Fehlermeldung ist jetzt Systemübergreifend nicht auf die
    Eingabebeschränkt

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -22,7 +22,7 @@ public class ConnectFour {
         while (board.getGameOver() == '.') {
             try {
                 int width = System.in.read();
-                if (width == 10) {
+                if (width < 20) {
                     continue;
                 }
                 if (width < 49 || width > 57) {
@@ -33,7 +33,7 @@ public class ConnectFour {
                 } else {
                     System.out.println("Did you even play this game once?");
                 }
-            } catch (ClassCastException e1) {
+            } catch (ClassCastException e1) {            	
                 System.out.println("Only the numbers shown on the game board are allowed!");
             } catch (IOException e2) {
                 System.out.println("The System threw an exception based on your input. What the heck are you doing?");
